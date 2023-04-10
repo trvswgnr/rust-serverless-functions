3bd046096e33ebb37d6d11b160d04c8ec03506c8
Travis Wagner <travis.wagner@umusic.com>
2023-04-10 17:43:50 -0400

    show query

--- api/[slug].rs
+++ api/[slug].rs
@@ -8,8 +8,17 @@ async fn main() -> Result<(), Error> {
 
 pub async fn handler(req: Request) -> Result<Response<Body>, Error> {
     let path = req.uri().path();
+    let query = req.uri().query();
     Ok(Response::builder()
         .status(StatusCode::OK)
         .header("Content-Type", "application/json")
-        .body(json!({ "path": path }).to_string().into())?)
+        .body(
+            json!({ "path": path,
+            "query": match query {
+                    None => "none",
+                    Some(q) => q,
+                }})
+            .to_string()
+            .into(),
+        )?)
 }
